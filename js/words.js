4098d307aa148c64b469f0372f51a8559ad0c7f2
skyplayguess-wordgame <94924743+skyplayguess-wordgame@users.noreply.github.com>
2022-07-07 22:36:47 +0530

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,52 @@
+let wordList = [
+  {
+    word: "skyplay",
+    hint: "Full name of skp",
+  },
+  {
+    word: "skp",
+    hint: "Listed On mexc global",
+  },
+  {
+    word: "meme",
+    hint: "Recent contest of skyplay ",
+  },
+  {
+    word: "cgp",
+    hint: "Coin grid daily bonus",
+  },
+  {
+    word: "coingrid",
+    hint: "Collecting coins",
+  },
+  {
+    word: "twitter",
+    hint: "more than 60k followers",
+  },
+  {
+    word: "telegram",
+    hint: "225k community ",
+  },
+  {
+    word: "sevenszeroes",
+    hint: "0000000 guess mod name",
+  },
+  {
+    word: "mbti",
+    hint: "trader in his name, from skp team",
+  },
+  {
+    word: "intel",
+    hint: "related to computer comapny partnership",
+  },
+  {
+    word: "rose",
+    hint: "red flower is most active in tg group chat",
+  },
+  {
+    word: "kaneshiro",
+    hint: "his name ends with |skyplay.io in tg group",
+  },
+
+  {},
+];
